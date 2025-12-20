380055a90df19554ef2121bcd8610b83a32cbf6e
Orney2009 <orlando.dodaho@epitech.eu>
2025-12-20 18:53:14 +0100

    fix: corrects frontend according to the new response format

--- website/front_view/src/components/Enterprises.vue
+++ website/front_view/src/components/Enterprises.vue
@@ -9,7 +9,7 @@
                 <div class="font-bold text-xl mb-2">{{ e.name }}</div>
                 <span class="inline-flex items-center bg-brand-softer border border-brand-subtle text-fg-brand-strong text-xs font-medium px-1.5 py-0.5 rounded-sm my-2">
                   <svg class="w-3 h-3 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.122 17.645a7.185 7.185 0 0 1-2.656 2.495 7.06 7.06 0 0 1-3.52.853 6.617 6.617 0 0 1-3.306-.718 6.73 6.73 0 0 1-2.54-2.266c-2.672-4.57.287-8.846.887-9.668A4.448 4.448 0 0 0 8.07 6.31 4.49 4.49 0 0 0 7.997 4c1.284.965 6.43 3.258 5.525 10.631 1.496-1.136 2.7-3.046 2.846-6.216 1.43 1.061 3.985 5.462 1.754 9.23Z"/></svg>
-                  {{ props.categories[e.category_id-1]["name"] }}
+                  {{ e.category }}
                 </span>
                 <p>
                     <span class="inline-flex items-center bg-brand-softer border border-brand-subtle text-fg-brand-strong text-xs font-medium px-1.5 py-0.5 rounded-sm my-2">
@@ -32,10 +32,8 @@
 
 <script setup>
 
-const props = defineProps({
-  toDisplay: Object,
-  entreprises : Array,
-  categories : Object
+const props = defineProps({  
+  entreprises : Array,  
 })
 
 
